41f7871960036455f58531c81841ec43c52ef289
Ashutosh <ashutoshchikhaliya@gmail.com>
2024-10-18 16:37:45 +0530

    setting up employee and admin page after login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login.jsx'
 import EmployeeDashboard from ".//components/Dashboard/EmployeeDashboard.jsx"
 import AdminDashboard from './components/Dashboard/AdminDashboard.jsx'
@@ -7,6 +7,19 @@ import { getLocalStorage, setLocalStorage } from './utils/LocalStorage.jsx'
 
 const App = () => {
 
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email, password) => {
+    if (email === "admin@admin.com" && password === "123") {
+      setUser("admin")
+      console.log(user)
+    } else if (email === "user@user.com" && password === "123") {
+      setUser("employee")
+      console.log(user)
+    } else {
+      alert("invalid credentials")
+    }
+  }
   useEffect(() => {
     // setLocalStorage()
     getLocalStorage()
@@ -17,9 +30,8 @@ const App = () => {
 
   return (
     <>
-      <Login />
-      {/* <EmployeeDashboard /> */}
-      {/* <AdminDashboard /> */}
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
